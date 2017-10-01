d6efbca7d665df577a9b5335db35e69369355b4d
Kasun Gamage <kasun.gamage@webstasy.one>
2017-10-01 19:31:44 +0530

    Insert is done. Delete is Done. Added ReadMe, .gitignore and test directory

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  /*This script is written focussing on the execution efficiency rather than coding standards, readability and maintainability*/
+  /*This script is written focussing on the performance rather than coding standards, readability and maintainability*/
   require_once 'config.php';
 
   $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE_NAME);
@@ -7,7 +7,27 @@
   $method = $_SERVER['REQUEST_METHOD'];
 
   function buildInsertQuery($dbTable, $params){
-    return 'INSERT into ' . $dbTable .'(' . $params[0] . ') VALUES(' . $params[1] . ')';
+    return 'INSERT INTO ' . $dbTable .'(' . $params[0] . ') VALUES(' . $params[1] . ')';
+  }
+  function buildUpdateQuery($dbTable, $params, $id){
+    global $conn;
+    $rs = $conn->query("SHOW INDEX FROM $dbTable WHERE Key_name = 'PRIMARY'");
+    $setList = array();
+    foreach ($params as $key => $value) {
+      /*If the value is a String without quotes add the quotes*/
+      if(is_string($value) && $value[0] != '\'' && $value[0] != '\"'){
+        $value = '\'' . $value . '\'';
+      }
+      $setQ = $key . '=' . $value;
+      array_push($setList, $setQ);
+    }
+    return 'UPDATE ' . $dbTable . ' SET ' . implode($setList, ',') . ' WHERE ' . $rs->fetch_assoc()['Column_name'] . '=' . $id;
+
+  }
+  function buildDeleteQuery($dbTable, $params, $id){
+    global $conn;
+    $rs = $conn->query("SHOW INDEX FROM $dbTable WHERE Key_name = 'PRIMARY'");
+    return 'DELETE FROM ' . $dbTable . ' WHERE ' . $rs->fetch_assoc()['Column_name'] . '=' . $id;
   }
 
   function processParams($arr){
@@ -27,10 +47,13 @@
   switch ($method) {
   case 'PUT':
     echo 'EDIT';
+    $params = processParams($_PUT);
+    $conn->query(buildUpdateQuery('test', $_PUT, 22));
     break;
   case 'POST':
     echo 'CREATE';
     $params = processParams($_POST);
+    $conn->query(buildInsertQuery('test', $params));
     break;
   case 'GET':
     echo 'VIEW';
@@ -38,6 +61,8 @@
     break;
   case 'DELETE':
     echo 'DELETE';
+    $params = processParams($_DELETE);
+    $conn->query(buildDeleteQuery('test', $params));
     break;
   case 'HEAD':
     echo 'E';
@@ -49,7 +74,6 @@
     handle_error($request);
     break;
   }
-  $conn->query(buildInsertQuery('test', $params));
   echo mysqli_error($conn);
   $conn->close();
 ?>
